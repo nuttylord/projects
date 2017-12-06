9069d0a165e1ef9cc35175772e1ebd4972a36a02
nutt <lawless423@hotmail.com>
2017-12-06 23:53:22 +0000

    Added VR Simulator and controllable character (BROKEN)
    Current State:
     - Player Unit falls with gravity and glitches through floor.
     - Player I J K L controls for movement still not working.
     - Character can move with VR Headset within play area.
     - Issue with gravity persists when movement script is disabled.

---
+++ RemnantsPOC/Assets/RemnantsGame/Scripts/PlayerInputsIJKL.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerInputsIJKL : MonoBehaviour {
+    //Simple movement script taken and adapted from https://docs.unity3d.com/ScriptReference/CharacterController.Move.html 
+    //Variables
+    public float speed = 6.0F;
+    //public float jumpSpeed = 8.0F;
+    //public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        // is the controller on the ground?
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            
+            // Mathf.Sqrt(2) is needed to calculate the diagonal distance properly.
+
+            //if(Input.GetKeyDown(KeyCode.I) && Input.GetKeyDown(KeyCode.J))
+            //{ }
+            if (Input.GetKeyDown(KeyCode.I)) 
+            {
+                moveDirection.x *= speed;
+                moveDirection.z = 0;
+            }
+            else if (Input.GetKeyDown(KeyCode.K))
+            {
+                moveDirection.x *= -speed;
+                moveDirection.z = 0;
+            }
+            else if (Input.GetKeyDown(KeyCode.J))
+            {
+                moveDirection.x = 0;
+                moveDirection.z *= -speed;
+            }
+            else if (Input.GetKeyDown(KeyCode.L))
+            {
+                moveDirection.x = 0;
+                moveDirection.z *= speed;
+            } 
+
+                
+            //moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            //moveDirection *= speed;
+            //Jumping
+            //if (Input.GetButton("Jump"))
+            //    moveDirection.y = jumpSpeed;
+
+        }
+        //Applying gravity to the controller
+        //moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime); // time is multiplied here so it doesnt need to happen earlier. 1 call of Time.deltaTime
+    }
+}
